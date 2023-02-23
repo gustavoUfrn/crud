b27dd81ecc515120f192d63216d8f67ffd7222b5
Gustavo <mxgugaa@gmail.com>
2023-02-23 16:49:52 -0300

    first commit

---
+++ App.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for, request, jsonify
+
+app = Flask(__name__)
+
+list = []
+id = 0
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+
+    global id
+    id += 1
+    user_id = id
+    name = request.form['name']
+    email = request.form['email']
+    tel = request.form['telefone']
+    cep = request.form['cep']
+    log = request.form['log']
+
+    list.append({'user_id': user_id, 'userName': name, 'userEmail': email, 'userPhone': tel, 'userCEP': cep, 'userLog': log})
+
+    return jsonify({'id': user_id, 'name': name, 'email': email, 'telefone': tel, 'cep': cep, 'log': log})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/form.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    $('form').on('submit', function (event) {
+        $.ajax({
+            data: {
+                name: $('#nameInput').val(),
+                email: $('#emailInput').val(),
+                telefone: $('#telInput').val(),
+                cep: $('#cepInput').val(),
+                log: $('#logInput').val(),
+            },
+            type: 'POST',
+            url: '/process'
+        })
+            .done(function (data) {
+                if (data.error) {
+                    $('#errorAlert').text(data.error).show();
+                    $('#sucessAlert').hide();
+                }
+                else {
+                    let tbody = document.getElementById('tbody')
+
+                    let tr = tbody.insertRow();
+
+                    let td_id = tr.insertCell();
+                    let td_name = tr.insertCell();
+                    let td_email = tr.insertCell();
+                    let td_telefone = tr.insertCell();
+                    let td_cep = tr.insertCell();
+                    let td_log = tr.insertCell();
+                    let td_action_button = tr.insertCell();
+
+                    let delete_button = document.createElement('button');
+                    delete_button.innerHTML = "Deletar"
+                    delete_button.className = ("class", "btn btn-danger")
+                    delete_button.setAttribute("onclick", data.id)
+
+                    let edit_button = document.createElement('button');
+                    edit_button.innerHTML = "Editar"
+                    edit_button.className = ("class", "btn btn-warning")
+                    edit_button.setAttribute("onclick", data.id)
+
+                    td_id.innerText = data.id;
+                    td_name.innerText = data.name;
+                    td_email.innerText = data.email;
+                    td_cep.innerText = data.cep;
+                    td_log.innerText = data.log;
+                    td_telefone.innerText = data.telefone;
+                    td_action_button.appendChild(delete_button)
+                    td_action_button.appendChild(edit_button)
+                }
+            });
+        event.preventDefault();
+    });
+}); 
